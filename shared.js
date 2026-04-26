cfb8fa58d66e3e08f24d332c1ef9120e29f96499
Flasmaa <ferdinand.rural@gmail.com>
2026-04-27 07:33:30 +0800

    Add files via upload

--- shared.js
+++ shared.js
@@ -1,20 +1,12 @@
 const sharedHtml = `
-<!--menu-->
 <div id="menu-placeholder"></div>
-<!-- -->
-<!--ico-->
 <link rel="icon" type="image/x-icon" href="thumbs up.ico">
-<!---->
-<!--ung css-->
 <link rel="stylesheet" href="styles.css">
-<!-- -->
-<!--header-->
 <div id="header-placeholder"></div>
-<!-- -->
 `;
 
 document.getElementById('shared-placeholder').innerHTML = sharedHtml;
-<!-- pang ayos ng code sabi ni youtube-->
+/* pang ayos ng code sabi ni youtube */
 const load = (src) => {
         const script = document.createElement('script');
         script.src = src;
