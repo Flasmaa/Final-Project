904b79548a7c034f8c83ae7d0d88648e05f3b869
Flasmaa <ferdinand.rural@gmail.com>
2026-04-27 13:29:19 +0800

    Add files via upload

--- shared/footer.js
+++ shared/footer.js
@@ -1,5 +1,5 @@
 const footerHtml = `
-  <div style="display: block; position: static; left: 0; bottom: 0; width: 100%; height: 143px; background-color: aliceblue; align-text: center; justify-content: center; text-align: center;">
+  <div style="display: block; position: static; left: 0; bottom: 0; width: 100%; height: 143px; background-color: aliceblue; align-text: center; justify-content: center; text-align: center; transform: translateX(-2%);">
     <dl>
       <dt><img src="img/phone.png" style="position: relative; width: 40px; height: 40px; margin-right: 2px; transform: translateY(12.5px)">Contact us:</dt>
       <dd>•NUMBER: <span style="font-size: 90%;">09617696470</span></dd>
--- shared/header.js
+++ shared/header.js
@@ -3,6 +3,9 @@ const headerHtml = `
   <div class="hoverfloat">
     <h1 style="display: flex; justify-content: center;">Papa Ferd's Printing Shop</h1>
   </div>
+  <div>
+    <img src="img/logo.png" style="position: absolute; width: 100px; height: 100px; margin-left: 91%; margin-top: -3.5%; padding: 0; border: solid 2px; border-color: black; border-radius: 50%;">
+  </div>
   <h2 style="display: flex; justify-content: center;">Printing Right By Your Door</h2>
 </div>
 `;
