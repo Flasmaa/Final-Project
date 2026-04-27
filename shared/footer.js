a45a9575be085631568d435ef98f9986c48e679e
Flasmaa <ferdinand.rural@gmail.com>
2026-04-27 08:51:07 +0800

    Add files via upload

--- shared/footer.js
+++ shared/footer.js
@@ -1,7 +1,7 @@
 const footerHtml = `
   <div style="display: block; position: static; left: 0; bottom: 0; width: 100%; height: 100px; background-color: aliceblue; align-text: center; justify-content: center; text-align: center;">
     <dl>
-      <dt>Contact us:</dt>
+      <dt><img src="img/phone.png" style="position: relative; width: 40px; height: 40px; margin-right: 2px; transform: translateY(12.5px)">Contact us:</dt>
       <dd>•NUMBER: <span style="font-size: 90%;">09617696470</span></dd>
       <dd>•EMAIL: <span style="font-size: 90%;">papsprinting@gmail.com</span></dd>
       <dd>•ADDRESS: <span style="font-size: 90%">0328 C, Baranggay Piti Piw Piw Wiw Wiw, Stop&Shop, Manila, Philippines</span></dd>
--- shared/menu.js
+++ shared/menu.js
@@ -2,16 +2,16 @@ const menuHtml = `
   <div style="display: block; position: fixed; width: 12%; height: 100%; background-color: aliceblue;">
     <dl>
         <div class="sec">
-            <dt><a href="index.html">Homepage</a></dt>
-        </div>
+            <dt><a href="index.html" class="default" style="color: black; text-decoration: none;">Homepage</a></dt>
+        </div><br><br>
         <div class="sec">
-            <dt><a href="product.html">Our Products</a></dt>
-        </div>
+            <dt><a href="product.html" class="default" style="color: black; text-decoration: none;">Our Products</a></dt>
+        </div><br><br>
         <div class="sec">
-            <dt><a href="preorder.html">Pre-Order</a></dt>
-        </div>
+            <dt><a href="preorder.html" class="default" style="color: black; text-decoration: none;" alt="Pre Order Now!"><img src="img/mail.png" style="position: relative; width: 40px; height: 40px; margin-right: 2px; transform: translateY(12.5px)"></img>Pre-Order</a></dt>
+        </div><br><br>
         <div class="sec">
-            <dt><img src="img/loc.jpg" style="position: relative; width: 15px; height: 15 px; margin-right: 2px; display: inline-block;"></img>Branches (TBA)</dt>
+            <dt><img src="img/loc.png" style="position: relative; width: 40px; height: 40px; margin-right: 2px; transform: translateY(10px)"></img>Branches (TBA)</dt>
         </div>
       <dd>•PUP Laboratory High School</dd>
       <dd>•Barangay Piti Piw Piw Wiw Wiw</dd>
